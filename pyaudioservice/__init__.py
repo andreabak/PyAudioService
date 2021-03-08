d3ddce0efdde855dd860b2f2ddf2c8d167c52399
abk16 <and.theunnamed@gmail.com>
2021-03-08 23:25:30 +0100

    Improved `AudioService.print_audio_devices()`

--- pyaudioservice/__init__.py
+++ pyaudioservice/__init__.py
@@ -1,6 +1,6 @@
 """Package with classes and modules related to audio acquisition, playback and recording"""
 
-__version__ = "0.1"
+__version__ = "0.1.1"
 
 from . import datatypes
 from . import service
